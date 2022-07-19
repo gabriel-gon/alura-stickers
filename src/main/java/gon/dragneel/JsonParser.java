920b5eba39543886ac027074f8d9afb014dee605
Gabriel Gonzalez Magalhães <gabrielgonzalez.95@gmail.com>
2022-07-19 07:46:55 -0300

    Adicionando versão melhorada que imprime as imagens dos filmes no terminal

--- src/main/java/gon/dragneel/JsonParser.java
+++ src/main/java/gon/dragneel/JsonParser.java
@@ -1,3 +1,5 @@
+package gon.dragneel;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -5,11 +7,20 @@ import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+/**
+ * Classe responsavel por pegar o texto do Json e transformar em uma lista de Maps String
+ *
+ */
 public class JsonParser {
 
     private static final Pattern REGEX_ITEMS = Pattern.compile(".*\\[(.+)\\].*");
     private static final Pattern REGEX_ATRIBUTOS_JSON = Pattern.compile("\"(.+?)\":\"(.*?)\"");
     
+    /**
+     * Metodo que recebe o Json em String e transforma em Uma Lista de Maps String
+     * @param json Json em String
+     * @return um List de Maps string
+     */
     public static List<Map<String, String>> parse(String json) {
 
         Matcher matcher = REGEX_ITEMS.matcher(json);
@@ -19,10 +30,10 @@ public class JsonParser {
 
         String[] items = matcher.group(1).split("\\},\\{");
 
-        List<Map<String, String>> dados = new ArrayList<>();
+        List<Map<String, String>> dados = new ArrayList<Map<String, String>>();
 
         for (String item : items) {
-            Map<String, String> atributosItem = new HashMap<>();
+            Map<String, String> atributosItem = new HashMap<String, String>();
 
             Matcher matcherAtributosJson = REGEX_ATRIBUTOS_JSON.matcher(item);
             while (matcherAtributosJson.find()) {
---
+++ src/main/java/gon/dragneel/Main.java
@@ -0,0 +1,63 @@
+package gon.dragneel;
+
+import java.awt.image.BufferedImage;
+import java.net.URI;
+import java.net.URL;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+
+import javax.imageio.IIOException;
+import javax.imageio.ImageIO;
+
+import tech.guiyom.anscapes.renderer.AbstractImageRenderer;
+import tech.guiyom.anscapes.renderer.RgbImageRenderer;
+import tech.guiyom.anscapes.renderer.TerminalImage;
+
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+    	
+    	//Obtendo as informações da API
+
+		//Properties properties = ConfigProperties.getProp();
+        //String url = "https://imdb-api.com/en/API/Top250Movies/"+properties.getProperty("prop.imdb.apiKey");
+    	
+        String url = "https://api.mocki.io/v2/549a5d8b";
+        
+        URI uri = URI.create(url);
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder(uri).GET().build();
+        HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+        String body = response.body();
+        
+        //Transforma o json recebido na requisição acima em uma lista de maps 
+        List<Map<String, String>> listaFilmes = JsonParser.parse(body);
+
+        //Percorre a lista com os filmes imprimindo as informações de cada um
+        for (Map<String,String> filme : listaFilmes) {
+            System.out.println("\u001b[1m Titulo: \u001b[m"+filme.get("title"));
+            
+            try {
+	            System.out.println("\u001b[1m Imagem: \u001b[m");
+	            URL urlImage = new URL(filme.get("image"));
+	    		BufferedImage sampleImage = ImageIO.read(urlImage );
+	    		AbstractImageRenderer converter = new RgbImageRenderer(25, 25);
+	    		TerminalImage x = converter.render(sampleImage);
+	    		System.out.println(x.getSequence());
+            }catch (IIOException e) {
+            	System.out.println("Imagem não encontrada");
+			}
+            
+            System.out.println("\u001b[1m Nota: \u001b[m"+filme.get("imDbRating")+"");
+            System.out.println("");
+        }
+        
+        
+        
+    }
+}
