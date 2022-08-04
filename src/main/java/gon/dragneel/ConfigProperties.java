846a4a9f122542e79229c69a6ba02d915c513e57
Gabriel Gonzalez Magalhães <gabrielgonzalez.95@gmail.com>
2022-08-04 14:37:00 -0300

    Update ConfigProperties.java

--- src/main/java/gon/dragneel/ConfigProperties.java
+++ src/main/java/gon/dragneel/ConfigProperties.java
@@ -1,1 +1,13 @@
+package gon.dragneel;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+public class ConfigProperties {
 
+	public static Properties getProp() throws IOException {
+		Properties props = new Properties();
+		InputStream file = ConfigProperties.class.getResourceAsStream("/config.properties");
+		props.load(file);
+		return props;
+	}
+} 
